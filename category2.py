1b046fdb518a185277ef7a16508c75f09df1974c
Luswis <97390837+luswisarya@users.noreply.github.com>
2023-08-24 17:02:54 +0700

    Create category2.py

---
+++ category2.py
@@ -0,0 +1,67 @@
+# ======================================================================
+# There are 5 questions in this exam with increasing difficulty from 1-5.
+# Please note that the weight of the grade for the question is relative
+# to its difficulty. So your Category 1 question will score significantly
+# less than your Category 5 question.
+#
+# Don't use lambda layers in your model.
+# You do not need them to solve the question.
+# Lambda layers are not supported by the grading infrastructure.
+#
+# You must use the Submit and Test button to submit your model
+# at least once in this category before you finally submit your exam,
+# otherwise you will score zero for this category.
+# ======================================================================
+#
+# Basic Datasets Question
+#
+# Create and train a classifier for the MNIST dataset.
+# Note that the test will expect it to classify 10 classes and that the 
+# input shape should be the native size of the MNIST dataset which is 
+# 28x28 monochrome. Do not resize the data. Your input layer should accept
+# (28,28) as the input shape only. If you amend this, the tests will fail.
+#
+
+import tensorflow as tf
+import urllib.request
+import numpy as np
+
+def solution_model():
+    mnist = tf.keras.datasets.mnist
+
+    # NORMALIZE YOUR IMAGE HERE
+    (training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+    training_images=training_images.reshape(60000, 28, 28, 1)
+    training_images=training_images / 255.0
+    test_images = test_images.reshape(10000, 28, 28, 1)
+    test_images=test_images/255.0
+
+    # DEFINE YOUR MODEL HERE
+    model = tf.keras.models.Sequential([
+      tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28, 1)),
+      tf.keras.layers.MaxPooling2D(2, 2),
+      tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+      tf.keras.layers.MaxPooling2D(2, 2),
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(128, activation='relu'),
+    # End with 10 Neuron Dense, activated by softmax
+      tf.keras.layers.Dense(10, activation='softmax')
+      ])
+    # COMPILE MODEL HERE
+    model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+    # TRAIN YOUR MODEL HERE
+    model.fit(training_images, training_labels, validation_data=(test_images, test_labels), epochs=3, verbose=1)
+
+    return model
+
+
+# Note that you'll need to save your model as a .h5 like this.
+# When you press the Submit and Test button, your saved .h5 model will
+# be sent to the testing infrastructure for scoring
+# and the score will be returned to you.
+
+if __name__ == '__main__':
+    model = solution_model()
+    model.save("mymodel.h5")
